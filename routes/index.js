4ce0444a95f83ae98f520b435415b27a926dd0d3
Laura Kapitza <laura.kapitza@gmail.com>
2021-05-06 23:29:09 +0200

    iteration 3

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => {
     const data = {
@@ -10,10 +12,20 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/movies', (req, res, next) => {
-    const data = {
-        bodyClass: "movies"
-    }
-    res.render('movies', data)
+    Movie.find()
+    .then(MoviesFromDB => {
+        const data = {
+            bodyClass: "movies",
+            movies: MoviesFromDB
+        };
+        console.log(data.movies)
+        res.render('movies', data);
+    })
+    .catch(err => {
+        console.log('An error occured while retrieving movies from the DB: ', err);
+        next(err);
+    });
+    
 
 });
 

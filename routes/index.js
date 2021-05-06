b08dddf01d25a56d6a25079de6e05513ea5e17af
Laura Kapitza <laura.kapitza@gmail.com>
2021-05-06 22:33:01 +0200

    fix hero image so it appears only on the home page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    const data = {
+        bodyClass: "home"
+    }
+    res.render('index', data)
+});
 
-router.get('/movies', (req, res, next) => res.render('movies'));
+router.get('/movies', (req, res, next) => {
+    const data = {
+        bodyClass: "movies"
+    }
+    res.render('movies', data)
+
+});
 
 module.exports = router;
